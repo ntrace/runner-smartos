28544b40e8b4263b9dea5c3e98df6c6f74ce5e64
Pedro Teixeira <pedro.teixeira@gmail.com>
2013-09-23 16:02:56 +0100

    direct script inside dtrace

--- scripts/dtrace.sh
+++ scripts/dtrace.sh
@@ -2,12 +2,4 @@
 
 set -e
 
-./run.js &
-NODE_PID=$!
-kill -STOP $NODE_PID
-
-/usr/sbin/dtrace -q -c ./run.js -n 'profile-97/execname == "node" && arg1/{ @[jstack(150,8000)] = count(); }' | /opt/local/bin/c++filt | /usr/local/bin/stackcollapse &
-DTRACE_PID=$!
-sleep 4
-
-kill -CONT $NODE_PID
+/usr/sbin/dtrace -q -c ./run.js -n 'profile-97/execname == "node" && arg1/{ @[jstack(150,8000)] = count(); }' | /opt/local/bin/c++filt | /usr/local/bin/stackcollapse
